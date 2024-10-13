cfe2b1f26097bb43de1783e218d1ea2f71bd953a
Vivek Kumar <vivekk12112@gmail.com>
2024-10-13 21:13:36 +0530

    'uploading'

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: ["<span> Hi, I am Vivek Kumar </span> Working in Frontend End Development , Enthusiastic Technology with a demonstrated history of learning in the information technology and services industry. Skilled in Back End Tech, JAVA , Web development with multiple tools and technologies . B.Tech focused in Information Technology from IIMT COLLEGE OF ENGINEERING , GREATER NOIDA"],
+      typeSpeed: 1,
+      contentType: 'html'
+      
+    });
+  }, 1000);
